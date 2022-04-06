4bcae2ef9989477d562f67b89fbd658868758a13
Brendan-funk <brendanzfunk@gmail.com>
2022-04-06 21:45:26 +0000

    finnishes the category admin page functionality

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CategoriesController < ApplicationController
-  http_basic_authenticate_with :name ENV['ADMIN_USERNAME'], password: ['ADMIN_PASSWORD']
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'].to_s, password: ENV['ADMIN_PASSWORD'].to_s
 
   def index
     @categories = Category.all
