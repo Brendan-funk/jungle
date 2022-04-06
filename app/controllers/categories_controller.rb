eb500be3ba95ad1accc30727cbe5be7cd9a67af2
Brendan-funk <brendanzfunk@gmail.com>
2022-04-06 22:20:57 +0000

    adds the user login feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-
+  #uses code from products controller but tweaked to be categories
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
